497eefe641644ee90f685972da284a1d0b186a02
yoongibum <yunki1212@nate.com>
2024-12-07 09:55:07 +0900

    1. 개발환경 crawling 파일 위치 2. views 불필요한 소스 제거

--- common_api/settings/development.py
+++ common_api/settings/development.py
@@ -155,8 +155,8 @@ CORS_ALLOW_HEADERS = (
 )
 
 GLOBAL_VARIABLE.update({
-    "yes24ExcelPath": "C:\github\crawling\crawling\excel\yes24_result.xlsx",
-    "koboExcelPath": "C:\github\crawling\crawling\excel\kyobo_result.xlsx",
-    "ypbooksExcelPath": "C:\github\crawling\crawling\ypbooks_result.xlsx",
-    "aladinExcelPath": "C:\github\crawling\crawling\aladin_result.xlsx"
+    "yes24ExcelPath": "C:\\github\\crawling\\crawling\\excel\\yes24_result.xlsx",
+    "koboExcelPath": "C:\\github\\crawling\\crawling\\excel\\kyobo_result.xlsx",
+    "ypbooksExcelPath": "C:\\github\\crawling\\crawling\\excel\\ypbooks_result.xlsx",
+    "aladinExcelPath": "C:\\github\\crawling\\crawling\\excel\\aladin_result.xlsx"
 })
